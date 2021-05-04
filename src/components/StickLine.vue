8c8c6daad2c252871442f3f61bf61e8cfc0d1fc7
HomyeeKing <HomyeeKing@gmail.com>
2021-05-04 17:58:14 +0800

    refactor: stick-line

---
+++ src/components/StickLine.vue
@@ -0,0 +1,39 @@
+<script lang='ts' setup>
+import { defineProps } from '@vue/runtime-core'
+
+defineProps({
+  main: String,
+  secondary: String,
+})
+
+</script>
+
+<template>
+  <div class="stick d-flex align-center">
+    {{ main }}
+  </div>
+</template>
+<style lang="stylus" scoped>
+.stick
+    position relative
+    font-family: PingFangSC-Medium;
+    color: #252525;
+    font-weight bold
+
+    &::before
+        content ''
+        display inline-block
+        margin-right 10px
+        width 4px
+        height 20px
+        background #E54A30
+        border-radius 2px
+    &::after
+        content v-bind(secondary)
+        position absolute
+        right 0
+        opacity: 0.6;
+        font-family: PingFangSC-Regular;
+        font-size: 13px;
+        color: #000000;
+</style>
--- src/views/Coupons.vue
+++ src/views/Coupons.vue
@@ -1,36 +1,39 @@
 <script lang='ts' setup>
 import axios from '@/axios'
 import CouponCard from '@/components/CouponCard.vue'
+import StickLine from '@/components/StickLine.vue'
 const { coupons } = await axios.get<any, {coupons: any}>('/api/coupons')
 
 </script>
 
 <template>
   <div class="panel">
-    <div class="stick">
-      微信支付优惠券
+    <div class="part-1">
+      <StickLine main="微信支付优惠券" secondary="'微信支付自动抵扣'" />
+      <coupon-card
+        v-for="item of coupons"
+        :key="item"
+        :name="item.name"
+        :tip="item.tip"
+        :type="item.type"
+        :remain="item.remain"
+        :note="item.note"
+        :left-part="{icon:item.icon,threshold:item.threshold,icontip:item.icontip}"
+      >
+        <template v-if="!item.note" #note>
+          <span style="margin-right: 5px;">{{ `${item.HKD}港币=${item.RMB}人民币` }}</span>
+          <small class="text-gray">
+            市场参考价
+          </small>
+          <small class="text-gray text-deleted">
+            {{ item.RMBE }}人民币
+          </small>
+        </template>
+      </coupon-card>
     </div>
-
-    <coupon-card
-      v-for="item of coupons"
-      :key="item"
-      :name="item.name"
-      :tip="item.tip"
-      :type="item.type"
-      :remain="item.remain"
-      :note="item.note"
-      :left-part="{icon:item.icon,threshold:item.threshold,icontip:item.icontip}"
-    >
-      <template v-if="!item.note" #note>
-        <span style="margin-right: 5px;">{{ `${item.HKD}港币=${item.RMB}人民币` }}</span>
-        <small class="text-gray">
-          市场参考价
-        </small>
-        <small class="text-gray text-deleted">
-          {{ item.RMBE }}人民币
-        </small>
-      </template>
-    </coupon-card>
+    <div class="part-2">
+      <StickLine main="商户优惠券" secondary="'请向收银员出示使用优惠券'" />
+    </div>
   </div>
 </template>
 <style lang="stylus" scoped>
@@ -40,28 +43,5 @@ const { coupons } = await axios.get<any, {coupons: any}>('/api/coupons')
     border-top-left-radius 25px
     padding 20px 10px
     overflow auto
-
-    .stick
-        position relative
-        font-family: PingFangSC-Medium;
-        color: #252525;
-        font-weight bold
-
-        &::before
-            content ''
-            display inline-block
-            margin-right 10px
-            width 4px
-            height 20px
-            background #E54A30
-            border-radius 2px
-        &::after
-            content '微信支付自动抵扣'
-            position absolute
-            right 0
-            opacity: 0.6;
-            font-family: PingFangSC-Regular;
-            font-size: 13px;
-            color: #000000;
 
 </style>
