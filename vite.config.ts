f8ee716114ec62e996bd2f60e502796e1d894534
HomyeeKing <HomyeeKing@gmail.com>
2021-05-11 11:52:09 +0800

    feat: vite-plugin-imock

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import path from 'path'
-import { defineConfig, PluginOption } from 'vite'
+import { defineConfig } from 'vite'
+import Mocker from 'vite-plugin-imock'
 import vue from '@vitejs/plugin-vue'
 import ViteComponents, {
   VantResolver,
@@ -7,45 +8,6 @@ import ViteComponents, {
 
 const resolve = (name: string) => path.resolve(__dirname, name)
 
-function mocker(): PluginOption {
-  let mockjs
-
-  try {
-    const resolved = require.resolve('mockjs')
-    mockjs = require(resolved)
-  }
-  catch (e) {
-    throw new Error('vite-plugin-mocker requires mockjs to be present in the dependency tree.')
-  }
-  function clearRequireCache() {
-    Object.keys(require.cache).forEach((key) => {
-      delete require.cache[key]
-    })
-  }
-  return {
-    name: 'vite-plugin-mocker',
-    enforce: 'pre',
-    configureServer(server) {
-      server.middlewares.use((req, res, next) => {
-        if (req.url.startsWith('/api')) {
-          const key = resolve(`mocker${req.url}`)
-          try {
-            clearRequireCache()
-            // eslint-disable-next-line @typescript-eslint/no-var-requires
-            const data = require(key)()
-            res.setHeader('Content-Type', 'application/json')
-            res.end(JSON.stringify(mockjs.mock(data)))
-            return
-          }
-          catch (error) {
-            throw new Error(error)
-          }
-        }
-        next()
-      })
-    },
-  }
-}
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -55,7 +17,7 @@ export default defineConfig({
         VantResolver(),
       ],
     }),
-    mocker(),
+    Mocker(),
   ],
   resolve: {
     alias: {
